c6db6482064f3ec010161a32e91ff03e7290d22d
Lucien Perouze <lucien.perouze@cezemb.re>
2022-06-17 15:08:20 +0200

    Remove Api from fronts

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,7 @@
 import './index.scss';
-import env, { Env, NativeEnv } from './state/env';
-import {
-  Model,
-  PaginatedList,
-  FormErrors,
-  ApiErrorData,
-  ApiError,
-  isApiError,
-  RequestBody,
-  buildRequestBody,
-  RequestParams,
-  buildRequestConfig,
-  apiGet,
-  apiPost,
-  apiPut,
-  apiDelete,
-  ApiConfig,
-  ApiContext,
-  useApi,
-  ApiProvider,
-} from './state/api';
 import { doesEventTargetContainsElements, useClickOutside } from './ui/interactions';
 import useDOMRect from './ui/domRect';
 import { useScrollProgress, useScrollThreshold, useScrollThresholds } from './ui/scrollProgress';
-import Img, {
-  Dimension,
-  Format,
-  Image,
-  useImageDimensions,
-  AspectRatio,
-  Orientation,
-  resolveRatio,
-  calcHeight,
-  calcWidth,
-} from './ui/image';
 import { BreakPoint, useBreakPoint } from './ui/container';
 import Wrapper, { WrapperProps } from './ui/wrapper';
 import {
@@ -60,38 +28,6 @@ import {
 import Place from './utils/place';
 
 /**
- * State
- */
-
-export { env };
-
-export type { Env, NativeEnv };
-
-export {
-  buildRequestBody,
-  buildRequestConfig,
-  apiGet,
-  apiPost,
-  apiPut,
-  apiDelete,
-  useApi,
-  ApiProvider,
-  isApiError,
-  ApiError,
-};
-
-export type {
-  Model,
-  PaginatedList,
-  FormErrors,
-  ApiErrorData,
-  RequestBody,
-  RequestParams,
-  ApiConfig,
-  ApiContext,
-};
-
-/**
  * UI
  */
 
@@ -104,22 +40,12 @@ export {
   useScrollThresholds,
   BreakPoint,
   useBreakPoint,
-  useImageDimensions,
-  resolveRatio,
-  calcHeight,
-  calcWidth,
-  Img,
   Wrapper,
   ModalsContext,
   useModals,
 };
 
 export type {
-  Dimension,
-  Image,
-  Format,
-  AspectRatio,
-  Orientation,
   WrapperProps,
   Modal,
   ModalComponentProps,
