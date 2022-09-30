3b5aea29920a5429d6bf933f8ada2a407bba07b0
Lucien Perouze <lucien.perouze@cezemb.re>
2022-09-30 18:13:02 +0200

    Fix typing

--- src/ui/domRect.ts
+++ src/ui/domRect.ts
@@ -1,10 +1,10 @@
-import { useState, useEffect, RefObject, useRef } from 'react';
+import { useState, useEffect, RefObject, useRef, MutableRefObject } from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
 
 export default function useDOMRect<T extends Element>(
-  ref: RefObject<T>,
-): DOMRect | DOMRectReadOnly | undefined {
-  const [DOMRect, setDOMRect] = useState<DOMRect | DOMRectReadOnly | undefined>();
+  ref: MutableRefObject<T | undefined> | RefObject<T | undefined>,
+): DOMRect | undefined {
+  const [DOMRect, setDOMRect] = useState<DOMRect | undefined>();
 
   const resizeObserver = useRef<ResizeObserver | undefined>();
 
