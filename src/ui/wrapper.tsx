c3636f0a1ef3c599158d1d6aba6c4872b1ee09f6
Lucien Perouze <lucien.perouze@cezemb.re>
2022-11-15 15:27:43 +0100

    More anchor props

--- src/ui/wrapper.tsx
+++ src/ui/wrapper.tsx
@@ -5,6 +5,8 @@ import {
   ReactNode,
   forwardRef,
   ForwardedRef,
+  HTMLAttributeAnchorTarget,
+  HTMLAttributeReferrerPolicy,
 } from 'react';
 import { NavLink, To } from 'react-router-dom';
 
@@ -16,7 +18,9 @@ export interface WrapperProps {
   disabled?: boolean;
   type?: 'submit' | 'reset' | 'button';
   href?: string;
-  target?: string;
+  rel?: string | undefined;
+  target?: HTMLAttributeAnchorTarget;
+  referrerPolicy?: HTMLAttributeReferrerPolicy | undefined;
 }
 
 export interface Props extends WrapperProps {
@@ -26,12 +30,31 @@ export interface Props extends WrapperProps {
 
 export default forwardRef<HTMLAnchorElement | HTMLButtonElement | HTMLDivElement, Props>(
   function Wrapper(
-    { children, className, to, onClick, onFocus, onBlur, disabled, type, href, target }: Props,
+    {
+      children,
+      className,
+      to,
+      onClick,
+      onFocus,
+      onBlur,
+      disabled,
+      type,
+      href,
+      target,
+      rel,
+      referrerPolicy,
+    }: Props,
     ref: ForwardedRef<HTMLAnchorElement | HTMLButtonElement | HTMLDivElement>,
   ): ReactElement {
     if (to) {
       return (
-        <NavLink ref={ref as ForwardedRef<HTMLAnchorElement>} to={to} className={className}>
+        <NavLink
+          ref={ref as ForwardedRef<HTMLAnchorElement>}
+          to={to}
+          className={className}
+          target={target}
+          rel={rel}
+          referrerPolicy={referrerPolicy}>
           {children}
         </NavLink>
       );
@@ -56,6 +79,8 @@ export default forwardRef<HTMLAnchorElement | HTMLButtonElement | HTMLDivElement
           ref={ref as ForwardedRef<HTMLAnchorElement>}
           href={href}
           target={target}
+          rel={rel}
+          referrerPolicy={referrerPolicy}
           className={className}>
           {children}
         </a>
